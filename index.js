11670a6e7644fc2dffb2107d475e44b5e0e07533
Denni Afredo <denni.afredo@ifg-life.id>
2023-05-06 22:33:23 +0700

    fix vercel

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const errorHandler = require('./middlewares/errorHandler')
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URL);
 const database = mongoose.connection;
 
 database.on('error', (error) => {
